eb5077db4b01253bc8f79060cc6acf5ada35f1c1
kyesu-developer <kyesu.developer@gmail.com>
2021-07-04 19:55:54 +0530

    all changes are reverted, today work is safe

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -8,8 +8,4 @@ export class AppPage {
   getTitleText(): Promise<string> {
     return element(by.css('app-root .content span')).getText() as Promise<string>;
   }
-
-  getTitleTextExgme(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
-  }
 }
